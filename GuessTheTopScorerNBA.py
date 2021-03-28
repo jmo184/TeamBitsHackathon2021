6e552869d09765f2cb4d33b24af1d32d17a46879
ksi616 <80421248+ksi616@users.noreply.github.com>
2021-03-27 20:48:59 -0500

    Update GuessTheTopScorerNBA.py
    Added code to make a .txt file for when user inputs an answer for each day but still needs to update it soon where it does not overwrite if user inputs a second answer the same day. Also fixed issue temporarily where if the name of the top scorer is not just a first and last name and includes an abbreviation, it will change the spot in the array to not read

--- GuessTheTopScorerNBA.py
+++ GuessTheTopScorerNBA.py
@@ -2,8 +2,14 @@
 # CHOOSE AN NBA PLAYER THAT MAY SCORE
 # THE HIGHEST AMOUNT OF POINTS TODAY
 
+
 from selenium import webdriver
 from datetime import datetime
+from os import path
+from datetime import datetime
+
+import time
+from tkinter import *
 
 now = datetime.now()
 time_string = now.strftime("%H %M %S")
@@ -13,9 +19,24 @@ totalTime = time_cut[0] + time_cut[1]
 
 userGuess = input("Who will score the most points in the NBA today? ")
 
-if int(totalTime) < 2300:
-    print("NBA Games are still in progress...")
-    quit()
+x = totalTime
+
+
+today = datetime.now()
+
+
+todaydate = datetime.today().strftime('%m-%d')
+
+name = "guessOn"+todaydate+".txt"
+
+
+f = open(name, "w")
+
+
+f.write(userGuess)
+
+
+print(name)
 
 if int(time_cut[0]) > 12:
     timeHour = int(time_cut[0]) - 12
@@ -23,25 +44,34 @@ if int(time_cut[0]) > 12:
 options = webdriver.ChromeOptions()
 
 # USE FOR OPENING WINDOW #
-#options.add_experimental_option("useAutomationExtension", False)
-#options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option("useAutomationExtension", False)
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
 # USE FOR HIDDEN WINDOW #
 options.add_argument('headless')
 
 driver_path = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=driver_path, options=options)
-driver.get('https://www.google.com/search?q=top+scorer+in+nba+today&source=hp&ei=U0hdYIb2G47-tAXnnr3QDA&iflsig=AINFCbYAAAAAYF1WYwDsgIPg1MPPULoPprYkOd78wvhP&oq=top+scorer+in+nba+t&gs_lcp=Cgdnd3Mtd2l6EAMYAjICCAAyAggAMgIIADIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOggIABCxAxCDAToLCC4QsQMQxwEQowI6BQgAELEDOggILhCxAxCTAjoFCC4QsQM6DgguELEDEIMBEMcBEKMCOggILhDHARCjAjoOCC4QsQMQxwEQowIQkwI6CAguEMcBEK8BOgoIABCxAxCDARAKOhAILhCxAxDHARCvARAKEJMCOggIABCxAxDJAzoFCAAQkgM6BAgAEAo6BQgAEIYDUJOsJlj72CZgwegmaANwAHgAgAF4iAGSC5IBBDIwLjGYAQCgAQGqAQdnd3Mtd2l6sAEA&sclient=gws-wiz')
+driver.get(
+    'https://www.google.com/search?q=top+scorer+in+nba+today&source=hp&ei=U0hdYIb2G47-tAXnnr3QDA&iflsig=AINFCbYAAAAAYF1WYwDsgIPg1MPPULoPprYkOd78wvhP&oq=top+scorer+in+nba+t&gs_lcp=Cgdnd3Mtd2l6EAMYAjICCAAyAggAMgIIADIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOggIABCxAxCDAToLCC4QsQMQxwEQowI6BQgAELEDOggILhCxAxCTAjoFCC4QsQM6DgguELEDEIMBEMcBEKMCOggILhDHARCjAjoOCC4QsQMQxwEQowIQkwI6CAguEMcBEK8BOgoIABCxAxCDARAKOhAILhCxAxDHARCvARAKEJMCOggIABCxAxDJAzoFCAAQkgM6BAgAEAo6BQgAEIYDUJOsJlj72CZgwegmaANwAHgAgAF4iAGSC5IBBDIwLjGYAQCgAQGqAQdnd3Mtd2l6sAEA&sclient=gws-wiz')
 
-nameAndPoints = driver.find_element_by_xpath('/html/body/div[8]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div[1]/div[3]/div/div[1]/div/div/div[1]').text
-dateOfScore = driver.find_element_by_xpath('/html/body/div[8]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div[1]/div[2]/div[1]/span[3]/span').text
+nameAndPoints = driver.find_element_by_xpath(
+    '/html/body/div[8]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div[1]/div[3]/div/div[1]/div/div/div[1]').text
+dateOfScore = driver.find_element_by_xpath(
+    '/html/body/div[8]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div[1]/div[2]/div[1]/span[3]/span').text
 
-#driver.close()
+# driver.close()
 
 nameSplit = nameAndPoints.split()
 
 topScorer = nameSplit[0] + " " + nameSplit[1]
-totalPoints = nameSplit[3]
+
+
+if nameSplit[3] == '·':
+    totalPoints = nameSplit[4]
+else:
+    totalPoints = nameSplit[3]
+
 
 print(topScorer + " was the Top Scorer in the NBA today with " + totalPoints + " points on " + dateOfScore)
 
